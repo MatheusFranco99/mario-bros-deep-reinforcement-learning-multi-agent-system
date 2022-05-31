76efe0d206b81b30382cd651780dfb3a51bbced8
Matheus Franco <matheusguilherme99@tecnico.usliboa.pt>
2022-05-31 15:36:28 +0100

    aa

--- dqn.py
+++ dqn.py
@@ -1,8 +1,20 @@
-from time import sleep
-from pettingzoo.atari import mario_bros_v3
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import optim
 
+class DQNNet(nn.Module):
+    def __init__(self,state_shape,action_shape,lr=1e-3):
 
-env = mario_bros_v3.env()
+        super(DQNNet,self).__init__()
+        self.dense1 = nn.Linear(in_features=state_shape,out_features=64)
+        self.dense2 = nn.Linear(in_features=64, out_features=64)
+        self.dense3 = nn.Linear(in_features=64,out_featurs=action_shape)
+        
+        self.optimizer = optim.Adam(self.parameters(),lr = lr)
 
-height, width, channels = env.observation_space.shape
-actions = env.action_space.n
+        def forward(self,x):
+            x = F.relu(self.dense1(x))
+            x = F.relu(self.dense2(x))
+            x = self.dense3(x)
+            return x
