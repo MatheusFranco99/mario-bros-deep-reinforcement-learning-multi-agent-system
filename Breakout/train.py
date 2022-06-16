39289c7bda352ec4fa1660db8be2c5e93399d6ae
MatheusFranco99 <mguilherme99@hotmail.com>
2022-06-16 15:20:39 +0100

    a

--- Breakout/train.py
+++ Breakout/train.py
@@ -5,17 +5,26 @@ from stable_baselines3 import PPO, DQN
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 
-env_name = 'Breakout-v4'
-env = gym.make(env_name)
-
-env_name = 'Breakout-v4'
+env_name = 'BreakoutNoFrameskip-v4'
 env = gym.make(env_name)
 env = gym.wrappers.AtariPreprocessing(env,frame_skip=1)
 env = DummyVecEnv([lambda: env])
-model = DQN('MlpPolicy', env, verbose=1)
+model = DQN('MlpPolicy', env, buffer_size = 100000, verbose=1)
 
 
-model.learn(total_timesteps=20000)
+model.learn(total_timesteps=4000000)
 
 
-model.save('dqn_breakout model')
+model.save('dqn_breakout 4000000 model')
+
+obs = env.reset
+
+env1 = gym.make(env_name)
+
+while True:
+	action = model.predict(obs)
+	obs, reward, done, info = env.step(action)
+	env1.step(action)
+	env1.render()
+	if done:
+		break
